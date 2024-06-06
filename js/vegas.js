442f5f2e9dd7bd2273bbe05b29d5c04a51818c4b
Mao Sasakura <huuchitaro@gmail.com>
2024-06-06 15:09:44 +0900

    make my own page

--- js/vegas.js
+++ js/vegas.js
@@ -1,9 +1,11 @@
 $(function() {
     $('.mainimg-tate').vegas({
         slides: [
-            { src: './images/tate_1.jpg' },	//1枚目の写真指定
-            { src: './images/tate_2.jpg' },	//2枚目の写真指定
-            { src: './images/tate_3.jpg' },	//3枚目の写真指定
+            { src: './images/elisabeth_tate.jpeg' },
+            { src: './images/friends_tate.jpeg' },
+            { src: './images/sanka_tate.jpeg' },
+            { src: './images/tango_tate.jpeg' },
+            { src: './images/moonriver_tate.jpeg' },
         ],
 		transition: 'blur',			//https://vegas.jaysalvat.com/documentation/transitions/から好みのtransitionを選んで置き換えられます。
 		animation: 'kenburns',		//https://vegas.jaysalvat.com/documentation/transitions/から好みのanimationを選んで置き換えられます。
@@ -17,9 +19,11 @@ $(function() {
 $(function() {
     $('.mainimg-yoko').vegas({
         slides: [
-            { src: './images/yoko_1.jpg' },	//1枚目の写真指定
-            { src: './images/yoko_2.jpg' },	//2枚目の写真指定
-            { src: './images/yoko_3.jpg' },	//3枚目の写真指定
+            { src: './images/elisabeth_yoko.jpeg' },	//1枚目の写真指定
+            { src: './images/friends_yoko.jpeg' },	//2枚目の写真指定
+            { src: './images/sanka_yoko.jpeg' },	//3枚目の写真指定
+            { src: './images/tango_yoko.jpeg' },
+            { src: './images/moonriver_yoko.jpeg' },
         ],
 		transition: 'blur',			//https://vegas.jaysalvat.com/documentation/transitions/から好みのtransitionを選んで置き換えられます。
 		animation: 'kenburns',		//https://vegas.jaysalvat.com/documentation/transitions/から好みのanimationを選んで置き換えられます。
